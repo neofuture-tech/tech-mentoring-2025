268c1ec9bea5b0d15975acd976ede8716a487462
Mikhael D'Amato <damato578@gmail.com>
2025-10-10 13:59:32 -0300

    feat: add create new enterprises with POST method by fetch, form and modal

--- aula-02/desafio-pratico/parte-3/frontend/script.js
+++ aula-02/desafio-pratico/parte-3/frontend/script.js
@@ -1,4 +1,7 @@
 const tableBody = document.querySelector(".container table tbody");
+const modalCreateEnterprise = document.querySelector(".create-enterprise");
+
+var currentOpenedModal = null;
 
 /**
  * @param { 'POST' | 'GET' | 'PUT' | 'DELETE' } method
@@ -8,15 +11,28 @@ async function api(method, body = undefined) {
     const options = {
         method,
         body: body ? JSON.stringify(body) : "",
+        headers: {
+            "Content-Type": "application/json",
+        },
     };
 
     if (!["POST", "PUT"].includes(method)) delete options.body;
 
     const result = await fetch("http://localhost:3000/api/empresas", options);
+    const data = await result.json();
 
-    return await result.json();
+    return {
+        status: result.status,
+        data,
+    };
 }
 
+// ======== TABLE METHODS ========
+
+/**
+ * @param {string} nome
+ * @param {number} funcionarios
+ */
 function createRow(nome, funcionarios) {
     const tableRow = document.createElement("tr");
     const nomeTableData = document.createElement("td");
@@ -40,7 +56,7 @@ function createLoadingRow() {
 async function loadTableRows() {
     tableBody.dataset.loading = true;
     createLoadingRow();
-    const empresaList = await api("GET");
+    const empresaList = (await api("GET")).data;
 
     tableBody.innerHTML = "";
     if (empresaList) {
@@ -51,6 +67,97 @@ async function loadTableRows() {
         });
     }
     tableBody.dataset.loading = false;
+}
+
+// ======== MODAL METHODS ========
+
+/**
+ * @param {PointerEvent} event
+ */
+function closeModal(event) {
+    if (currentOpenedModal) {
+        clearModalErrors(currentOpenedModal);
+        clearModalInputs(currentOpenedModal);
+        return currentOpenedModal.removeAttribute("data-opened");
+    }
+
+    event.target.parentNode.removeAttribute("data-opened");
+}
+
+/**
+ * @param {HTMLElement} target
+ */
+function openModal(target) {
+    currentOpenedModal = target;
+    target.setAttribute("data-opened", "");
+}
+
+function showNewEnterprise() {
+    openModal(modalCreateEnterprise);
+}
+
+/**
+ * @param {HTMLElement} modal
+ */
+function clearModalInputs(modal) {
+    modal.querySelectorAll("input").forEach((input) => (input.value = ""));
+}
+
+function clearModalErrors(modal) {
+    modal
+        .querySelectorAll(".input-field")
+        .forEach((input) => delete input.dataset.error);
+    modal
+        .querySelectorAll("form")
+        .forEach((input) => delete input.dataset.error);
+}
+
+/**
+ * @param {SubmitEvent} event
+ */
+async function modalSubmit(event) {
+    event.preventDefault();
+    let totalErrors = 0;
+    const form = event.target.parentNode;
+    const nomeEl = form.querySelector("#nome");
+    const funcionariosEl = form.querySelector("#funcionarios");
+    const nome = nomeEl.value;
+    const funcionarios = funcionariosEl.value;
+
+    clearModalErrors(currentOpenedModal);
+
+    if (!nome || nome.trim().length == 0) {
+        nomeEl.parentNode.setAttribute("data-error", "");
+        nomeEl.value = nomeEl.value.trim();
+        totalErrors += 1;
+    }
+
+    if (
+        !funcionarios ||
+        funcionarios.trim().length == 0 ||
+        Number(funcionarios) <= 0
+    ) {
+        funcionariosEl.parentNode.setAttribute("data-error", "");
+        funcionariosEl.value = funcionariosEl.value.trim();
+        totalErrors += 1;
+    }
+
+    if (totalErrors > 0) {
+        return;
+    }
+
+    const result = await api("POST", {
+        nome,
+        funcionarios,
+    });
+
+    if (result.status == 201) {
+        clearModalInputs(form.parentNode.parentNode);
+        form.parentNode.parentNode.removeAttribute("data-opened");
+    }
+
+    form.setAttribute("data-error", "");
+    form.querySelector(".form-error").innerText = result.data.message;
 }
 
 async function main() {
